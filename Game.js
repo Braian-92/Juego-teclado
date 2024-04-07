be88cbedb053039d22b1fe234f3bd68a568783e3
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-06 22:24:39 -0300

    letras por colores en texto 3d

--- Game.js
+++ Game.js
@@ -97,75 +97,72 @@ class Game {
   
 
   crearTexto(text) {
-
-    // let text = 'Evelyn',
     let bevelEnabled = true,
-    font = undefined,
-    fontName = 'optimer', // helvetiker, optimer, gentilis, droid sans, droid serif
-    fontWeight = 'bold'; // normal bold
+        font = undefined,
+        fontName = 'optimer', // helvetiker, optimer, gentilis, droid sans, droid serif
+        fontWeight = 'bold'; // normal bold
     
     const depth = 0.1,
-    size = 10,
-    hover = .1,
-
-    curveSegments = 10,
-
-    bevelThickness = 1, // profundidad del texto en 3D
-    bevelSize = .1;
+        size = 10,
+        hover = .1,
+        curveSegments = 10,
+        bevelThickness = 1, // profundidad del texto en 3D
+        bevelSize = .1;
 
     let escenaPrincipal = this.scene;
+    let letras = []; // Array para almacenar las letras individuales
+    let separacionLetras = 2; // Separación entre letras
 
     const loader = new FontLoader();
     loader.load( './libs/three/examples/fonts/' + fontName + '_' + fontWeight + '.typeface.json', function ( response ) {
+        font = response;
 
-      font = response;
+        let materialsPorLetra = []; // Array para almacenar materiales por letra
 
-      console.log(font);
-      console.log(text);
+        for (let i = 0; i < text.length; i++) {
+            // Crear material con color aleatorio
+            let color = Math.random() * 0xffffff;
+            let material = new THREE.MeshPhongMaterial({ color: color, flatShading: true });
+            materialsPorLetra.push(material);
+        }
 
-      let materials = [
-        new THREE.MeshPhongMaterial( { color: 0xd30364, flatShading: true } ), // front
-        new THREE.MeshPhongMaterial( { color: 0x36012b } ) // side
-      ];
+        let totalWidth = 0;
 
-      let textGeo = new TextGeometry( text, {
+        // Crear y posicionar cada letra
+        for (let i = 0; i < text.length; i++) {
+            let letraGeo = new TextGeometry( text[i], {
+                font: font,
+                size: size,
+                depth: depth,
+                curveSegments: curveSegments,
+                bevelThickness: bevelThickness,
+                bevelSize: bevelSize,
+                bevelEnabled: bevelEnabled
+            } );
 
-        font: font,
+            // Crear la malla de la letra con el material correspondiente
+            let letraMesh = new THREE.Mesh( letraGeo, materialsPorLetra[i] );
 
-        size: size,
-        depth: depth,
-        curveSegments: curveSegments,
+            // Calcular el desplazamiento horizontal de la letra
+            letraGeo.computeBoundingBox();
+            let letterWidth = letraGeo.boundingBox.max.x - letraGeo.boundingBox.min.x;
+            letraMesh.position.x = totalWidth + (i > 0 ? separacionLetras : 0); // Separación entre letras
+            totalWidth += letterWidth + separacionLetras; // Acumular el ancho total
+            letraMesh.position.z = 0;
 
-        bevelThickness: bevelThickness,
-        bevelSize: bevelSize,
-        bevelEnabled: bevelEnabled
+            // Agregar la letra a la escena y al array
+            escenaPrincipal.add( letraMesh );
+            letras.push(letraMesh);
+        }
 
-      } );
+        // Calcular el centro del texto
+        const centerOffset = -0.5 * totalWidth;
 
-      textGeo.computeBoundingBox();
-
-      const centerOffset = - 0.5 * ( textGeo.boundingBox.max.x - textGeo.boundingBox.min.x );
-
-      let textMesh1 = new THREE.Mesh( textGeo, materials );
-
-      textMesh1.position.x = centerOffset;
-      // textMesh1.position.y = hover;
-      textMesh1.position.z = 0;
-
-      // textMesh1.rotation.x = 0;
-      // textMesh1.rotation.y = Math.PI * 2;
-
-      escenaPrincipal.add( textMesh1 );
-
-    } );
-
-
-
-			
-
-    
-
-    
+        // Centrar el grupo de letras
+        letras.forEach(letraMesh => {
+            letraMesh.position.x += centerOffset;
+        });
+    });
   }
 
   agregarElementos() {
